8a48a00d2dc6e1f618c760852323184c77955ea6
Patrick Klostermann <dev@netflair.at>
2018-04-25 23:06:15 +0200

    removed not working line for input reset

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ var sum = document.getElementById('sum');
 
 document.getElementById('calcInput').addEventListener('keypress', function calc(e) {
     var key = e.keyCode;
-    //Calculate on "Enter"(13), show result in "sum" and clear input
+    //Calculate on "Enter"(13), show result in "sum"
     if (key === 13) { 
       var calcInput = document.getElementById('calcInput').value;
       sum.innerHTML += calcInput + "=" + eval(calcInput) + "<br>";
-      calcInput.value = '';
     }
 
 });
