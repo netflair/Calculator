824981ae147935267045430330b644def0d94d58
Patrick Klostermann <dev@netflair.at>
2018-04-26 09:11:40 +0200

    added autoscroll for the results container and hideing the scrollbar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var sum = document.getElementById('sum');
+//Hide Scrollbar in result box(sum)
+sum.style.paddingRight = sum.offsetWidth - sum.clientWidth + "px";
 
 document.getElementById('calcInput').addEventListener('keypress', function calc(e) {
     var key = e.keyCode;
+
     //Calculate on "Enter"(13), show result in "sum"
     if (key === 13) { 
       var calcInput = document.getElementById('calcInput').value;
+      var calcIn = document.getElementById('calcInput');
       sum.innerHTML += calcInput + "=" + eval(calcInput) + "<br>";
+      
+      //Autoscroll result box (sum)
+      sum.scrollTop = sum.scrollHeight;
     }
 
 });
